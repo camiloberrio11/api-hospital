366d5f7ad8c6996eb52b9327884dbf086ef00ca8
camiloberrio <camiloberriouribe@gmail.com>
2021-05-18 19:06:43 -0500

    Initialice hospital model

---
+++ routes/hospitals.routes.js
@@ -0,0 +1,35 @@
+const { Router } = require('express');
+// const { getUsers, createUser, updateUser, deleteUser } = require('../controllers/usuarios');
+const { validateFields } = require('../middlewares/validateFields');
+const { check } = require('express-validator');
+const { validateJwt } = require('../middlewares/validateJwt');
+const router = Router();
+/*
+  Ruta raiz: /api/hospitals
+ */
+
+router.get('/', validateJwt, getUsers);
+router.put(
+  '/:iduser',
+  [
+    validateJwt,
+    check('name', 'Nombre obligatorio').not().isEmpty(),
+    check('role', 'Rol obligatorio').not().isEmpty(),
+    check('email', 'email es obligatorio').isEmail(),
+    validateFields,
+  ],
+  updateUser
+);
+router.post(
+  '/',
+  [
+    check('name', 'Nombre obligatorio').not().isEmpty(),
+    check('password', 'Paswword obligatorio').not().isEmpty(),
+    check('email', 'email es obligatorio').isEmail(),
+    validateFields,
+  ],
+  createUser
+);
+router.delete('/:iduser', validateJwt, deleteUser);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const { dbConnection } = require('./database/config');
 const routesUser = require('./routes/usuarios.routes');
 const routesAuth = require('./routes/auth.routes');
+const routesHospitals = require('./routes/hospitals.routes');
 
 // Create server
 const app = express();
@@ -17,5 +18,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/users', routesUser);
 app.use('/api/auth', routesAuth);
+app.use('/api/hospitals', routesHospitals);
 
-app.listen(process.env.PORT, () => console.log('Servidor corriendo en el puerto 3000'));
+app.listen(process.env.PORT, () =>
+  console.log(`Servidor corriendo en el puerto ${process.env.PORT}`)
+);
